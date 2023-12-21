7ba30d1e505d9b3bcce91809aef923ac203e9efb
sui334 <fleuartz@gmail.com>
2023-12-22 04:47:32 +0900

    add new tests, fix tests name

--- src/main.rs
+++ src/main.rs
@@ -53,35 +53,42 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_qf_encode1() {
+    fn test_qf_encode1_normal() {
         let notation = "e2,d2h,f2,e8,e7v,e7";
         let result = qf_encode::encode(notation);
         assert_eq!(result, Ok("QGCLJPRA".to_string()));
     }
 
     #[test]
-    fn test_qf_encode2() {
+    fn test_qf_encode2_jump() {
         let notation = "e2,e2v,f2h,e8,h2h,e7,d6h,f7,f6h,e7,d2,d7,c7v,d8,d3,d3h,c3,b3h,a2h,d9,b3,c9,a3,c8,c5v,c2v,h6h,c7,a4,c6,a5,a5h,b5,b6h,a7v,b1v,c5,c4,c6,b4,b6,a4,a6";
         let result = qf_encode::encode(notation);
         assert_eq!(result, Ok("QrDMjUj0qyrWZvIAk2kYgGZk4sqvQECgKp8MEkBmZg".to_string()));
     }
 
     #[test]
-    fn test_qf_encode3() {
+    fn test_qf_encode3_long() {
         let notation = "e2,e2v,e3,d3h,e2,e8,d7v,e7,e6h,f7,d2,f8,e2,f9,e1,g9,d1,f9,e1,e9,f1,f9,e1,e9,f1,f9,e1,e9,f1,f9,e1h,f8,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,g1,f9,f1,e9,g1,f9,f1,e9,g1,f9,f1,e9,g1,f9,f1,f1v,e1,e9,d1,d9,c1,d8,c2,c8h,b2,b2h,a2,a8h,a3,a3v,a4,a4h,a3,d7,a2,d6,b2,d5,c2,d4,d2,c4,d3,c3,b3,d3,d2h,c3,b4,c4,d4,e4,f3v,e5,e4,f5,g4h,g6h,e5,g5,h1h,h5,f5,i5,g5,i4,g6,h4,h6,h3,i6,g1v,i7,g3,i8,g2,i9";
         let result = qf_encode::encode(notation);
         assert_eq!(result, Ok("SJDMCTRPNKwmAgQmYmImYiZiKERgJmImYiZiJmImYiZiJiJmImYiZiJsVmZmQLpolrgNAJhERCQkJgRii2ACLVAinq4ChyIiQGJCxgYEA".to_string()));
     }
 
     #[test]
-    fn test_qf_encode4() {
+    fn test_qf_encode4_diagonal() {
+        let notation = "e2,e8,e3,e7,e4,e6,e5,e6h,f6";
+        let result = qf_encode::encode(notation);
+        assert_eq!(result, Ok("QJBAQECsE".to_string()));
+    }
+
+    #[test]
+    fn test_qf_encode5_invalid() {
         let notation = "aaa,aaaa";
         let result = qf_encode::encode(notation);
         assert_eq!(result, Err(qf_encode::QFError::QFEncodeError));
     }
 
     #[test]
-    fn test_qf_decode1() {
+    fn test_qf_decode1_normal() {
         let notation = "e2,d2h,f2,e8,e7v,e7";
         let result = qf_encode::encode(notation);
         let notation_decoded = qf_decode::decode(&result.unwrap());
@@ -89,7 +96,7 @@ mod tests {
     }
 
     #[test]
-    fn test_qf_decode2() {
+    fn test_qf_decode2_jump() {
         let notation = "e2,e2v,f2h,e8,h2h,e7,d6h,f7,f6h,e7,d2,d7,c7v,d8,d3,d3h,c3,b3h,a2h,d9,b3,c9,a3,c8,c5v,c2v,h6h,c7,a4,c6,a5,a5h,b5,b6h,a7v,b1v,c5,c4,c6,b4,b6,a4,a6";
         let result = qf_encode::encode(notation);
         let notation_decoded = qf_decode::decode(&result.unwrap());
@@ -97,7 +104,7 @@ mod tests {
     }
 
     #[test]
-    fn test_qf_decode3() {
+    fn test_qf_decode3_long() {
         let notation = "e2,e2v,e3,d3h,e2,e8,d7v,e7,e6h,f7,d2,f8,e2,f9,e1,g9,d1,f9,e1,e9,f1,f9,e1,e9,f1,f9,e1,e9,f1,f9,e1h,f8,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,e1,f9,f1,e9,g1,f9,f1,e9,g1,f9,f1,e9,g1,f9,f1,e9,g1,f9,f1,f1v,e1,e9,d1,d9,c1,d8,c2,c8h,b2,b2h,a2,a8h,a3,a3v,a4,a4h,a3,d7,a2,d6,b2,d5,c2,d4,d2,c4,d3,c3,b3,d3,d2h,c3,b4,c4,d4,e4,f3v,e5,e4,f5,g4h,g6h,e5,g5,h1h,h5,f5,i5,g5,i4,g6,h4,h6,h3,i6,g1v,i7,g3,i8,g2,i9";
         let result = qf_encode::encode(notation);
         let notation_decoded = qf_decode::decode(&result.unwrap());
@@ -105,7 +112,15 @@ mod tests {
     }
 
     #[test]
-    fn test_qf_decode4() {
+    fn test_qf_decode4_diagonal() {
+        let notation = "e2,e8,e3,e7,e4,e6,e5,e6h,f6";
+        let result = qf_encode::encode(notation);
+        let notation_decoded = qf_decode::decode(&result.unwrap());
+        assert_eq!(notation_decoded, Ok(notation.to_string()));
+    }
+
+    #[test]
+    fn test_qf_decode5_invalid() {
         let result = "AAAAAAA".to_string();
         let notation_decoded = qf_decode::decode(&result);
         assert_eq!(notation_decoded, Err(qf_decode::QFError::QFDecodeError));
