718f0df1bbe3e666bd27085256f58160ded8bab1
sui334 <fleuartz@gmail.com>
2023-12-22 07:16:46 +0900

    add qf_code log

--- src/convert_html_to_qf.rs
+++ src/convert_html_to_qf.rs
@@ -1,10 +1,13 @@
 use crate::qf_encode::encode;
 use regex::Regex;
+use std::collections::HashSet;
 use std::error::Error;
 use std::fmt;
-use std::fs;
-use std::io::Read;
+use std::fs::{self, OpenOptions};
+use std::io::{Read, Seek, SeekFrom, Write};
 use colored::*;
+use crypto::md5::Md5;
+use crypto::digest::Digest;
 
 #[derive(Debug, PartialEq)]
 pub enum QFError {
@@ -23,13 +26,29 @@ pub fn convert(directory_path: &str) -> Result<(), Box<dyn Error>> {
     println!("{} \\{}", "Convert:".yellow().bold(), directory_path);
     let entries = fs::read_dir(directory_path).map_err(|_| QFError::QFFileError)?;
 
+    let log_file_path = format!("{}/qf_code.log", directory_path);
+    let mut log_file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(&log_file_path)
+        .map_err(|_| QFError::QFFileError)?;
+
+    let mut existing_hashes = HashSet::new();
+
+    let log_contents = fs::read_to_string(&log_file_path).unwrap_or_default();
+    for line in log_contents.lines() {
+        if let Some(md5_hash) = extract_md5_hash(line) {
+            existing_hashes.insert(md5_hash);
+        }
+    }
+
     for entry in entries {
         let entry = entry?;
         let file_path = entry.path();
 
         if let Some(file_path_str) = file_path.to_str() {
             if file_path_str.ends_with(".html") {
-                process_html_file(file_path_str)?;
+                process_html_file(file_path_str, &mut log_file, &mut existing_hashes)?;
             }
         }
     }
@@ -37,10 +56,30 @@ pub fn convert(directory_path: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn process_html_file(file_path: &str) -> Result<(), Box<dyn Error>> {
+fn extract_md5_hash(line: &str) -> Option<String> {
+    let re = Regex::new(r#"\[MD5: ([a-f0-9]+)\]"#).ok()?;
+    if let Some(captures) = re.captures(line) {
+        captures.get(1).map(|m| m.as_str().to_string())
+    } else {
+        None
+    }
+}
+
+fn process_html_file(
+    file_path: &str,
+    log_file: &mut fs::File,
+    existing_hashes: &mut HashSet<String>,
+) -> Result<(), Box<dyn Error>> {
     let mut file = fs::File::open(file_path).map_err(|_| QFError::QFFileError)?;
+
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
+
+    let mut hasher = Md5::new();
+    hasher.input_str(&contents);
+    let md5_hash = hasher.result_str();
+
+    file.seek(SeekFrom::Start(0))?;
 
     let re = Regex::new(r#"<script[^>]*>(?P<script>[\s\S]*?"quoridorstrats_notation":[\s\S]*?)</script>"#)?;
 
@@ -60,8 +99,20 @@ fn process_html_file(file_path: &str) -> Result<(), Box<dyn Error>> {
     let notations_str = notations.join(",");
 
     match encode(&notations_str) {
-        Ok(qf_code) => println!("{} <- ({})", qf_code, file_path),
-        Err(err) => eprintln!("{} encoding notations: {:?}", "Error:".red().bold(), err),
+        Ok(qf_code) => {
+            println!("{} <- ({})", qf_code, file_path);
+
+            if existing_hashes.contains(&md5_hash) {
+                return Ok(());
+            }
+
+            writeln!(log_file, "{} <- ({}) [MD5: {}]", qf_code, file_path, md5_hash)?;
+            existing_hashes.insert(md5_hash);
+        }
+        Err(err) => {
+            eprintln!("{} encoding notations: {:?}", "Error:".red().bold(), err);
+            writeln!(log_file, "{} encoding notations: {:?}", "Error:".red().bold(), err)?;
+        }
     }
 
     Ok(())
